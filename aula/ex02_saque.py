80a4b85810f79a42ba3648832e6fe3628f4f3730
vinicoloco <vinicius.inacio1997@gmail.com>
2022-03-30 09:00:03 -0300

    Novas Aulas

--- aula/ex02_saque.py
+++
@@ -1,6 +0,0 @@
-saque = int(input("Qual o calor que você deseja sacar?\n-->"))
-nota20 = saque // 20
-nota5 = saque % 20 // 5
-nota2 = saque % 20 // 2
-nota1 = saque % 20 % 5 % 2 // 1
-print("Você ira receber " + str(nota20) + " notas de 20, " + str(nota5) + " notas de cinco, " + str(nota2) + " notas de dois e " + str(nota1) + " notas de um.")
