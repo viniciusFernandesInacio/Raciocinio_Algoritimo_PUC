f21f7290e8bfc6af4033db614a1bc176b54d459b
vinicoloco <vinicius.inacio1997@gmail.com>
2022-05-24 15:47:51 -0300

    Nova exercício concluído.

--- Exercícios_para_entrega/Lista_4_Expressões/Exercício5_L4.py
+++ Exercícios_para_entrega/Lista_4_Expressões/Exercício5_L4.py
@@ -6,28 +6,50 @@ def cls():
 
 cls()
 
-esc = int(input('Digite um número inteiro para ser usado como escalar: '))
-lin = int(input('Digite o total de linhas da matriz: '))
-colu = int(input('Digite o total de colunas da matriz: '))
-matriz = []
-lista = []
+#Criando a primeira matriz.
+matriz1 = []
+lin = int(input('Informe o número de linhas da matriz 1: '))
+colu = int(input('Informe o número de colunas da matriz 1: '))
 for i in range(lin):
-    linha = []
+    matriz1.append([])
     for j in range(colu):
-        num = random.randint(0,10)
-        linha.append(num)
-        matriz.append(linha) 
-print(matriz)
+        matriz1[i].append(random.randint(0,5))
 
-esc = int(input('Digite um número inteiro para ser usado como escalar: '))
-lin = int(input('Digite o total de linhas da matriz: '))
-colu = int(input('Digite o total de colunas da matriz: '))
-matriz = []
-lista = []
+#Imprime a primeira matriz.
+for i in range(len(matriz1[i])):
+    for j in range (len(matriz1[i])):
+        print(matriz1[i][j], end = ' ' )
+    print('') 
+print('')
+
+#Criando a segunda matriz.
+matriz2 = []
+lin = int(input('Informe o número de linhas da matriz 2: '))
+colu = int(input('Informe o número de colunas da matriz 2: '))
 for i in range(lin):
-    linha = []
+    matriz2.append([])
     for j in range(colu):
-        num = random.randint(0,10)
-        linha.append(num)
-        matriz.append(linha) 
-print(matriz)
+        matriz2[i].append(random.randint(0,5))
+
+#Imprime a segunda matriz.
+for i in range(len(matriz2[i])):
+    for j in range (len(matriz2[i])):
+        print(matriz2[i][j], end = ' ' )
+    print('')  
+
+#Soma das duas matrizes.
+if(len(matriz1) != len(matriz2) or len(matriz1[0]) != len(matriz2[0])):
+    print('[ERRO] As matrizes devem ter o mesmo número de linhas e colunas.')
+res = []
+for i in range (len(matriz1)):
+    res.append([])
+    for j in range(len(matriz1[0])):
+        res[i].append(matriz1[i][j] + matriz2[i][j])
+else:
+    print('\nA soma das duas matrizes é igual a:\n')
+
+
+for i in range(len(res[i])):
+    for j in range (len(res[i])):
+        print(res[i][j], end = ' ' )
+    print('')      
